d36664669576f0c3036854fea9c10ceb5205f229
Balivern <123549102+Balivern@users.noreply.github.com>
2024-03-26 10:59:42 +0100

    animation de début de jeu + interface

--- Bubbleshooter.ino
+++ Bubbleshooter.ino
@@ -24,6 +24,8 @@ int en_tete = 0;  //en_tete du jeu
 volatile int pos = 20;  //position de la fleche d'envoi en x
 volatile int incl = 0;  //inclinaison de la fleche d'envoi (-1=gauche 0=droit 1=droite)
 
+volatile int score = 0; // score
+
 volatile int pos_cube_x = 0;    //position de la boule en bas a gauche en x
 volatile int pos_cube_y = 0;    //position de la boule en bas a gauche en y
 volatile int incl_cube = -1;    //inclinaison de la fleche d'envoi (-1=gauche 0=droit 1=droite)
@@ -40,11 +42,92 @@ volatile bool deplacement = false;  //booleen pour indiquer si la boule est en d
 void setup() {
   Serial.begin(9600);
   matrix.begin();
+  delay(1000); // nécessaire pour que l'animation de lancement du jeu ne se fasse qu'une fois 
   //Timer3.initialize(75000);//defini l'intervalle
   Timer3.initialize(1000);//deux 0 en plus
   Timer3.attachInterrupt(deplacer_cube);
+  init_anim(); // animation de lancement du jeu
+  init_interface(); // initialise l'interface de jeu
   initialisation_jeu();
   afficher_jeu();
+}
+
+void init_anim() {
+    static int x_letter = (matrix.width()/2)-3;
+    static int y_letter = (matrix.height()/2)-3;
+
+    static int x;
+    static int y;
+    static int dx;
+    static int dy;
+
+    for (int i = 0; i < 2; i++) { // répète la boucle d'animation, 2 fois
+      for (int k = 0; k < 3; k++) { // les leds de la matrice s'allument toutes, 3 fois
+        matrix.fillRect(0,0, matrix.width()-1, matrix.height()-1, matrix.Color888(255, 0, 0));
+        delay(50);
+        matrix.fillScreen(matrix.Color888(0, 0, 0)); // Efface l'écran
+      }
+
+      for (int j = 0; j < 2; j++) { // allume les leds en faisant le contour de la matrice, 2 fois
+        x = 1;
+        y = 0;
+        dx = 1;
+        dy = 0;     
+        matrix.drawChar(x_letter-(7*3), (matrix.height()/2)-8, 'B', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter-(7*2), (matrix.height()/2)-8, 'u', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter-7, (matrix.height()/2)-8, 'b', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter, (matrix.height()/2)-8, 'b', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter+7, (matrix.height()/2)-8, 'l', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter+(7*2), (matrix.height()/2)-8, 'e', matrix.Color888(255,0,0), 0, 1);
+
+        matrix.drawChar(x_letter-(7*3), (matrix.height()/2), 'S', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter-(7*2), (matrix.height()/2), 'h', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter-7, (matrix.height()/2), 'o', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter, (matrix.height()/2), 'o', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter+7, (matrix.height()/2), 't', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter+(7*2), (matrix.height()/2), 'e', matrix.Color888(255,0,0), 0, 1);
+        matrix.drawChar(x_letter+(7*3), (matrix.height()/2), 'r', matrix.Color888(255,0,0), 0, 1);
+
+        while (x!=0 || y!=0) {     // animation pixel tournant      
+          matrix.drawPixel(x, y, matrix.Color333(7, 7, 7)); // Allumer les pixels en blanc
+          x += dx;      // Mettre à jour les positions
+          y += dy;
+          if (x==0 && y==0) {     // Vérifier si la ligne a atteint un bord et changer de direction si nécessaire
+              dx = 1;
+              dy = 0;
+          }
+          else if (x==matrix.width()-1 && y==0) {
+              dx = 0;
+              dy = 1;
+          }
+          else if (x==matrix.width()-1 && y==matrix.height()-1) {
+              dx = -1;
+              dy = 0;
+          }
+          else if (x==0 && y==matrix.height()-1) {
+              dx = 0;
+              dy = -1;
+          }
+          delay(3);     // Contrôle de la vitesse de l'animation
+        }
+        matrix.fillScreen(matrix.Color888(0, 0, 0));
+      }
+    }
+}
+
+void init_interface() {
+  matrix.drawRect(0,0,matrix.width(), matrix.height(), matrix.Color333(7,7,0));
+  matrix.drawLine(8,matrix.height()-11,matrix.width()-1,matrix.height()-11, matrix.Color333(7,7,0));
+  matrix.drawLine(8,0,8,matrix.height()-11, matrix.Color333(7,7,0));
+
+  // score
+  score=0;
+  /*std::string t = to_string(score);
+  char const *n_char = t.c_str();
+  Serial.println(n_char[0]);*/
+  matrix.drawChar(matrix.width()-21, matrix.height()-9, '7', matrix.Color333(255,0,0), 0, 1);
+  matrix.drawChar(matrix.width()-14, matrix.height()-9, '9', matrix.Color333(255,0,0), 0, 1);
+  matrix.drawChar(matrix.width()-7, matrix.height()-9, '1', matrix.Color333(255,0,0), 0, 1); 
 }
 
 void afficher_jeu(){
