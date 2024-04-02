5e95c3704f0eb545467d50afc015a5fbf95e1f7e
maxueir <123549175+maxueir@users.noreply.github.com>
2024-04-02 22:29:19 +0200

    fin 3x3 debut collisions

--- Bubbleshooter.ino
+++ Bubbleshooter.ino
@@ -21,11 +21,12 @@
 DFRobot_RGBMatrix matrix(A, B, C, D, E, CLK, LAT, OE, false, WIDTH, _HIGH);
 
 //taille d'un bubble shooter : 17 de large et 16 de hauteur (17=> peerdu)
-int hauteur = 0;  //hauteur du jeu
+int hauteur = 1;  //hauteur du jeu
 int marge_g = 1;    //marge du jeu
 int marge_d = 1;    //marge du jeu
-int en_tete = 0;  //en_tete du jeu
+int en_tete = 1;  //en_tete du jeu
 
+int nb_couleur=7;
 
 volatile int pos = 30;  //position de la fleche d'envoi en x
 volatile int incl = 0;  //inclinaison de la fleche d'envoi (-1=gauche 0=droit 1=droite)
@@ -37,8 +38,6 @@ volatile int pos_cube_y = 0;    //position de la boule en bas a gauche en y
 volatile int incl_cube = 0;    //inclinaison de la fleche d'envoi (-1=gauche 0=droit 1=droite)
 volatile int couleur_cube = 1;  //indice de la couleur utilisee dans le tableau couleurs
 
-int largeur_jeu;
-int hauteur_jeu;
 short jeu[15][15];//que des 0 partout par defaut, donc aucune bille
 
 //DFRobot_RGBMatrix matrix(A, B, C, D, E, CLK, LAT, OE, false, WIDTH, _HIGH);
@@ -51,15 +50,15 @@ volatile bool deplacement = false;  //booleen pour indiquer si la boule est en d
 void setup() {
   matrix.begin();
   Serial.begin(9600);
-  //delay(1500); // nécessaire pour que l'animation de lancement du jeu ne se fasse qu'une fois 
+  delay(1100); // nécessaire pour que l'animation de lancement du jeu ne se fasse qu'une fois 
   //Serial.println("ici");
-  matrix.fillScreen(0);
+  //matrix.fillScreen(0);
   //Timer3.initialize(75000);//defini l'intervalle
-  Timer3.initialize(1000000);//deux 0 en plus
+  Timer3.initialize(10000);//deux 0 en plus
   Timer3.attachInterrupt(deplacer_cube);
   //init_anim(); // animation de lancement du jeu
   //init_interface(); // initialise l'interface de jeu
-  //initialisation_jeu();
+  initialisation_jeu();
   //delay(1000);
   //Serial.print("affichage");
   //afficher_jeu();
@@ -144,11 +143,10 @@ void init_interface() {
 }
 
 void afficher_jeu(){//3, fill, 1de marge en x 0 en y 
+  
+  for (int i = 0; i < 15; i++) {
+    for (int j = 0; j < 15; j++) {
 
-  for (int i = 0; i < hauteur_jeu; i++) {
-    for (int j = 0; j < largeur_jeu; j++) {
-      //Serial.println(couleurs[jeu[i][j]]);
-      //int aux=jeu[i][j];
       if(i%2==0){
         matrix.fillRect(marge_g+j*3 +j, en_tete+i*3 , 3, 3, couleurs[jeu[i][j]]);
       }
@@ -159,34 +157,20 @@ void afficher_jeu(){//3, fill, 1de marge en x 0 en y
       
     }
   }
-  //matrix.drawRect(marge_g, en_tete, 2, 2, couleurs[4]);
   
 
 }
 
 void initialisation_jeu(){
-if( 64-marge_d-marge_g%2==0){
-  marge_g=marge_g+1;
-}
-if( 64-hauteur-en_tete%2==0){
-  hauteur=hauteur+1;
-}
 
-largeur_jeu=(64-marge_d-marge_g)/2;
-hauteur_jeu=(64-hauteur-en_tete-5)/2;
-Serial.println(largeur_jeu);
-Serial.println(hauteur_jeu);
-
-  for (int i = 0; i < hauteur_jeu; i++) {
-    for (int j = 0; j < largeur_jeu; j++) {
-      //Serial.println("hey");
-      int aux=random(1,7);
-      jeu[i][j]=aux;
-      
-      //Serial.println(jeu[i][j]);
+  deplacer(1,0);
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 15; j++) {
+      jeu[i][j]=random(1,nb_couleur);
       
     }
   }
+  afficher_jeu();
 }
 
 bool estPossible(int a, int b) {  //a=dir b=incl => verifier si le deplacement est possible(ne pas sortir de la zone de jeu)
@@ -202,9 +186,19 @@ bool estPossible(int a, int b) {  //a=dir b=incl => verifier si le deplacement e
   }
 }
 
-bool case_libre() {
-
-  return pos_cube_y > 2 + en_tete;
+bool case_libre() {//indique si le cube peut avancer ou s'il vient de se bloquer
+  if(pos_cube_y%3==0){//si la prochaine case peut être un cube ou le bord
+    
+    int ligne=pos_cube_y/3;
+    int colonne=pos_cube_x/3;
+    //Serial.println(ligne);
+    //Serial.println(jeu[ligne][colonne]);
+    return (jeu[ligne][colonne]==0) && (pos_cube_y > 2 + en_tete);
+  }
+  else{//sinon
+    return true;
+  }
+  
 }
 
 void deplacer_cube() {
@@ -217,6 +211,20 @@ void deplacer_cube() {
           pos_cube_y = pos_cube_y - 1;
         }
       } else if (incl_cube == 1) {
+        if(pos_cube_x==59){
+          matrix.drawLine(pos_cube_x, pos_cube_y, pos_cube_x + 2, pos_cube_y, couleurs[couleur_cube * i]);
+          matrix.drawLine(pos_cube_x + 1, pos_cube_y - 1, pos_cube_x + 3, pos_cube_y - 1, couleurs[couleur_cube * i]);
+          matrix.drawLine(pos_cube_x, pos_cube_y - 2, pos_cube_x + 2, pos_cube_y - 2, couleurs[couleur_cube * i]);
+
+          if (i == 0) {
+          pos_cube_x = pos_cube_x + 1;
+          pos_cube_y = pos_cube_y - 1;
+          incl_cube=-1;
+        }
+        }
+        else{
+
+        
 
         matrix.drawLine(pos_cube_x, pos_cube_y, pos_cube_x + 2, pos_cube_y, couleurs[couleur_cube * i]);
         matrix.drawLine(pos_cube_x + 1, pos_cube_y - 1, pos_cube_x + 3, pos_cube_y - 1, couleurs[couleur_cube * i]);
@@ -227,14 +235,25 @@ void deplacer_cube() {
           pos_cube_y = pos_cube_y - 1;
           
         }
+        }
       } else if (incl_cube == -1) {
 
+        if(pos_cube_x==2){
+          matrix.drawLine(pos_cube_x, pos_cube_y, pos_cube_x + 2, pos_cube_y, couleurs[couleur_cube * i]);
+          matrix.drawLine(pos_cube_x - 1, pos_cube_y - 1, pos_cube_x + 1, pos_cube_y - 1, couleurs[couleur_cube * i]);
+          matrix.drawLine(pos_cube_x, pos_cube_y - 2, pos_cube_x + 2, pos_cube_y - 2, couleurs[couleur_cube * i]);
+
+          if (i == 0) {
+          pos_cube_x = pos_cube_x -1;
+          pos_cube_y = pos_cube_y - 1;
+          incl_cube=1;
+        }
+        }
+        else{
         matrix.drawLine(pos_cube_x, pos_cube_y, pos_cube_x + 2, pos_cube_y, couleurs[couleur_cube * i]);
         matrix.drawLine(pos_cube_x - 1, pos_cube_y - 1, pos_cube_x + 1, pos_cube_y - 1, couleurs[couleur_cube * i]);
         matrix.drawLine(pos_cube_x - 2, pos_cube_y - 2, pos_cube_x, pos_cube_y - 2, couleurs[couleur_cube * i]);
 
-        //matrix.drawLine(pos_cube_x+1,pos_cube_y-1, pos_cube_x+2, pos_cube_y-1, matrix.Color888(255, 0, 255));
-        //matrix.drawLine(pos_cube_x,pos_cube_y-2, pos_cube_x+1, pos_cube_y-2, matrix.Color888(255, 0, 255));
         if (i == 0) {
           pos_cube_x = pos_cube_x - 1;
           pos_cube_y = pos_cube_y - 1;
@@ -244,26 +263,29 @@ void deplacer_cube() {
           }
         }
       }
+      }
     }
   } else if (deplacement){
     deplacement = false;
-    couleur_cube = random(1, 7);
+    couleur_cube = random(1, nb_couleur);
     if (incl == 0) {
-      pos_cube_x = pos;
-      pos_cube_y = 63 - hauteur - 5;
-      matrix.drawRect(pos, matrix.height() - 7 - hauteur, 2, 2, couleurs[couleur_cube]);      //boule a envoyer
+      //pos_cube_x = pos;
+      //pos_cube_y = 63 - hauteur - 5;
+      matrix.fillRect(pos, matrix.height() - 8 - hauteur, 3, 3, couleurs[couleur_cube]);      //boule a envoyer
     }
     else if (incl == -1) {
-      pos_cube_x = pos - 5;
-      pos_cube_y = 63 - hauteur - 5;
-      matrix.drawLine(pos - 5, matrix.height() - 6 - hauteur, pos - 4, matrix.height() - 6 - hauteur, couleurs[couleur_cube ]);  //boule a envoyer en lignes horizontales
-      matrix.drawLine(pos - 6, matrix.height() - 7 - hauteur, pos - 5, matrix.height() - 7 - hauteur, couleurs[couleur_cube ]);
+      //pos_cube_x = pos - 5;
+      //pos_cube_y = 63 - hauteur - 5;
+      matrix.drawLine(pos - 5, matrix.height() - 6 - hauteur, pos - 3, matrix.height() - 6 - hauteur, couleurs[couleur_cube ]);  //boule a envoyer en lignes horizontales
+      matrix.drawLine(pos - 6, matrix.height() - 7 - hauteur, pos - 4, matrix.height() - 7 - hauteur, couleurs[couleur_cube ]);
+      matrix.drawLine(pos - 7, matrix.height() - 8 - hauteur, pos - 5, matrix.height() - 8 - hauteur, couleurs[couleur_cube ]);
     }
     else if (incl == 1) {
-      pos_cube_x = pos + 5;
-      pos_cube_y = 63 - hauteur - 5;
-      matrix.drawLine(pos + 6, matrix.height() - 6 - hauteur, pos + 5, matrix.height() - 6 - hauteur, couleurs[couleur_cube ]);  //boule a envoyer en lignes horizontales
-      matrix.drawLine(pos + 7, matrix.height() - 7 - hauteur, pos + 6, matrix.height() - 7 - hauteur, couleurs[couleur_cube ]);
+      //pos_cube_x = pos + 5;
+      //pos_cube_y = 63 - hauteur - 5;
+      matrix.drawLine(pos + 7, matrix.height() - 6 - hauteur, pos + 5, matrix.height() - 6 - hauteur, couleurs[couleur_cube ]);  //boule a envoyer en lignes horizontales
+      matrix.drawLine(pos + 8, matrix.height() - 7 - hauteur, pos + 6, matrix.height() - 7 - hauteur, couleurs[couleur_cube ]);
+      matrix.drawLine(pos + 9, matrix.height() - 8 - hauteur, pos + 7, matrix.height() - 8 - hauteur, couleurs[couleur_cube ]);
     }
   }
 }
