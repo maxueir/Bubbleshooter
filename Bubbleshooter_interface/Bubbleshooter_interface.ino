47ac4034e20c18ab1ddd5aef596625d53a700ead
Balivern <123549102+Balivern@users.noreply.github.com>
2024-04-28 17:19:48 +0200

    bubbleshooter OK

--- Bubbleshooter_interface/Bubbleshooter_interface.ino
+++ Bubbleshooter_interface/Bubbleshooter_interface.ino
@@ -41,7 +41,7 @@ void setup() {
   Serial.begin(9600);
   Serial2.begin(9600); // Initialise la communication série
   delay(1100);
-  //initAnim(); // animation de lancement du jeu
+  initAnim(); // animation de lancement du jeu
   initInterface(); // initialise l'interface de jeu
   Timer3.initialize(100000);
   Timer3.attachInterrupt(majScore);
@@ -49,7 +49,7 @@ void setup() {
   Timer4.attachInterrupt(dernierEssai);
 }
 
-void initAnim() {
+void initAnim() { // animation de lancement du jeu
   static int x_letter = (matrix.width() / 2) - 3;
   static int y_letter = (matrix.height() / 2) - 3;
 
@@ -109,7 +109,7 @@ void initAnim() {
   }
 }
 
-void initInterface() {
+void initInterface() { // interface pendant le choix de la difficulté par le joueur 
   static int x_letter = 2;
   static int y_letter = 2;
 
@@ -130,7 +130,7 @@ void initInterface() {
   
 }
 
-void majScore() {
+void majScore() { // met à jour la score en temps réel
   // effacement du rectangle contenant l'affichage du score
   if (ajout_score.toInt()>0) {
     ajout_score_temp = String(ajout_score.toInt() - 10);
@@ -157,7 +157,7 @@ void majScore() {
   }
 }
 
-void choixDifficulte() {
+void choixDifficulte() { // modifie la difficulté de la partie (variable "level") et la jauge de difficulté dans l'interface
   matrix.fillRect(2 + (7 * 5), 2, 8 * 3, 7, matrix.Color333(0, 0, 0));
   if (level==0) {
     matrix.fillRect(2 + (7 * 5), 2, 8 * (level + 1), 7, matrix.Color333(0, 7, 0));
@@ -170,23 +170,7 @@ void choixDifficulte() {
   }
 }
 
-void nbEssai() {
-  /*
-  String nb = String(coup_restant);
-  if (jeu_demarrer && coup_restant>1) {
-    matrix.drawChar(3, 25, nb[0], matrix.Color333(0, 0, 0), 0, 1);
-    nb = String(coup_restant);
-    matrix.drawChar(3, 25, nb[0], matrix.Color333(255, 0, 0), 0, 1);
-    if (coup_restant == 1) {
-      essai_visible = true;
-    }
-  }
-  else if (jeu_demarrer && coup_restant==0) {
-    matrix.drawChar(3, 25, nb[0], matrix.Color333(0, 0, 0), 0, 1);
-    coup_restant = 4 - level;
-    nb = String(coup_restant);
-    matrix.drawChar(3, 25, nb[0], matrix.Color333(255, 0, 0), 0, 1);
-  }*/
+void nbEssai() { // met à jour la nombre d'essai restant pour joueur
   if(level==0){
     if(coup_restant==4){
   for(int i=0;i<4;i++){
@@ -243,16 +227,7 @@ void nbEssai() {
   }
 }
 
-void dernierEssai() {
-  //Serial.println(essai_visible);
-  /*if (coup_restant == 1 && essai_visible) {
-    matrix.drawChar(3, 25, '1', matrix.Color333(255, 0, 0), 0, 1);
-    essai_visible = !essai_visible;
-  }
-  else if (coup_restant == 1 && !essai_visible) {
-    matrix.drawChar(3, 25, '1', matrix.Color333(0, 0, 0), 0, 1);
-    essai_visible = !essai_visible;
-  }*/
+void dernierEssai() { // fait clignoter le dernier rectangle en bas du nombre d'essai s'il ne reste qu'un seul essai au joueur (variable "coup_restant" = 1) et fait clignoter la prochaine bille de l'utilisateur dans le rectangle du bas 
   if(jeu_demarrer){
   if (essai_visible) {
     matrix.fillRect(35 , matrix.height() - 7, 3, 3, couleurs[file_couleurs[0]]);
@@ -289,8 +264,6 @@ void dernierEssai() {
       }
       
     }
-
-
   }
   essai_visible = !essai_visible;
 
@@ -298,7 +271,7 @@ void dernierEssai() {
 
 }
 
-void creerFile(){
+void creerFile(){ // créer une nouvelle file de couleur pour les billes
   for(int i=0;i<5;i++){
     file_couleurs[i]=random(1,nb_couleur);//creation de la file
     matrix.fillRect(35 + (5 * i), matrix.height() - 7, 3, 3, couleurs[file_couleurs[i]]);// affichage des carré à venir
@@ -308,12 +281,9 @@ void creerFile(){
   matrix.drawLine(28 - 3, matrix.height() - 6 - 3, 28, matrix.height() - 6, matrix.Color333(7, 7, 0));
   matrix.drawLine(28 - 3, matrix.height() - 6 + 3, 28, matrix.height() - 6, matrix.Color333(7, 7, 0));
   Serial2.print(file_couleurs[0]); // envoi de la couleur de la 1ere bille
-  /*for(int i=0;i<5;i++){
-    Serial.println(file_couleurs[i]);
-  }*/
 }
 
-int pop(){
+int pop(){ // renvoi le numéro de la couleur de la bille prochaine qu'aura le joueur une fois son coup joué 
   int res=file_couleurs[1];
   for(int i=0;i<5;i++){
     if(i==4){
@@ -349,7 +319,10 @@ void loop() {
       jeu_demarrer = false;
       coup_restant = 4 - level;
       score="0";
-      matrix.fillRect(4, 55, 57, 59, matrix.Color333(0, 0, 0));
+      // effacer le rectangle du nombre de coup restant
+      matrix.fillRect(2, 13, 7, 38, matrix.Color333(0, 0, 0));
+      // effacer le rectangle de la file de bille
+      matrix.fillRect(2, matrix.height()-9, 59, 7, matrix.Color333(0, 0, 0));
     }
     else {
       bool isInt = true; // Supposons que c'est un entier jusqu'à preuve du contraire
@@ -366,8 +339,8 @@ void loop() {
   }
   else if (Serial2.available() > 0 && !jeu_demarrer) { // vrai si o a reçu un caractère sur la liaison série
     char transmit = Serial2.read(); // met dans lu le caractère lu
-    Serial.print("Interface a reçu: ");
-    Serial.println(transmit); // Affiche le message reçu
+    //Serial.print("Interface a reçu: ");
+    //Serial.println(transmit); // Affiche le message reçu
 
     if (transmit=='q' && level>0) {
       level--;
@@ -379,8 +352,7 @@ void loop() {
     }
     else if (transmit == ' ') {
       jeu_demarrer = true;
-      coup_restant = 4 - level;
-      // affichage du nombre de coup restant en fonction de la difficulté choisit
+      coup_restant = 4 - level; // affichage du nombre de coup restant en fonction de la difficulté choisit
       String nb = String(coup_restant);
       String scor = "Score";
       for (int i = 0; i<scor.length(); i++) {
@@ -404,38 +376,20 @@ void loop() {
       creerFile();
 
       if(level==0){
-
-  
-  matrix.fillRect(2, 13, 7, 7, matrix.Color333(255, 255, 255));
-  matrix.fillRect(2, 23, 7, 7, matrix.Color333(255, 255, 255));
-  matrix.fillRect(2, 33, 7, 7, matrix.Color333(255, 255, 255));
-  matrix.fillRect(2, 43, 7, 7, matrix.Color333(255, 255, 255));
-  }
-  else if(level==1){
-  matrix.fillRect(2, 18, 7, 7, matrix.Color333(255, 255, 255));
-  matrix.fillRect(2, 28, 7, 7, matrix.Color333(255, 255, 255));
-  matrix.fillRect(2, 38, 7, 7, matrix.Color333(255, 255, 255));
-  }
-  else{
-  
-  matrix.fillRect(2, 23, 7, 7, matrix.Color333(255, 255, 255));
-  matrix.fillRect(2, 33, 7, 7, matrix.Color333(255, 255, 255));
-  }
+        matrix.fillRect(2, 13, 7, 7, matrix.Color333(255, 255, 255));
+        matrix.fillRect(2, 23, 7, 7, matrix.Color333(255, 255, 255));
+        matrix.fillRect(2, 33, 7, 7, matrix.Color333(255, 255, 255));
+        matrix.fillRect(2, 43, 7, 7, matrix.Color333(255, 255, 255));
+      }
+      else if(level==1){
+        matrix.fillRect(2, 18, 7, 7, matrix.Color333(255, 255, 255));
+        matrix.fillRect(2, 28, 7, 7, matrix.Color333(255, 255, 255));
+        matrix.fillRect(2, 38, 7, 7, matrix.Color333(255, 255, 255));
+      }
+      else{
+        matrix.fillRect(2, 23, 7, 7, matrix.Color333(255, 255, 255));
+        matrix.fillRect(2, 33, 7, 7, matrix.Color333(255, 255, 255));
+      }
     }
   }
 }
-
-/* 
-recu :
-- score (mise a jour)
-- command (pour le score, choix difficulté)
-
-envoi :
-- file de couleur de cube
-
-revoir interface pour afficher :
-- file de couleurs de cube suivant 
-- score
-- ajout de X points
-- diffulté
-*/
